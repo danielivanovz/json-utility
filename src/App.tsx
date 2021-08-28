ca4a4f4a2625e0e991291d09b163361e054d15e1
Daniel Ivanov <daniel.ivanov@me.com>
2021-08-29 00:27:18 +0200

    JSON validator

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { Success } from './components/Success';
 import { Error } from './components/Error';
 
+//TODO: Add Redux/Mobx SM
+
 const isJSONCheck = (input: string | undefined) => {
 	try {
 		JSON.parse(input!);
@@ -15,29 +17,43 @@ const isJSONCheck = (input: string | undefined) => {
 export const App = () => {
 	const [query, setQuery] = useState<string | undefined>('');
 	const [isCopied, setState] = useState<boolean>(false);
-	const [isError, setError] = useState<boolean>(false);
+	const [isJSON, setError] = useState<boolean>(false);
+	const [json, setJson ] = useState('{}')
 	const inputRef = useRef<HTMLTextAreaElement>(null);
+	
 
 	const updateQuery = () => {
 		const inputText = inputRef.current;
-		setQuery(inputText?.value);
+		if( isJSON ) {
+			console.log(isJSON)
+			JSON.stringify(JSON.stringify(JSON.parse(query!)))
+		}
+		
+		setQuery(inputText?.value!);
 	};
+
+	const handlePaste = (e: ClipboardEvent): any => {
+		const paste = e.clipboardData!.getData('Text').replace(/\s*$/,'');
+		isJSONCheck(paste) ? setQuery(paste) : setError(false)
+	}
 
 	useEffect(() => {
 		setState(false);
 		try {
 			JSON.parse(query!);
-			setError(false);
-		} catch (error) {
+			setJson(query!)
+			JSON.stringify(JSON.stringify(JSON.parse(query!)))
 			setError(true);
+		} catch (error) {
+			setError(false);
 		}
-	}, [query]);
+	}, [inputRef, query]);
 
 	return (
 		<div className='flex flex-col p-14 h-screen items-center justify-center bg-gray-50 '>
 			<div className='absolute right-0 top-0 max-h-10 flex font-FiraMono p-6 mt-2 items-center'>
 				<p className='mx-2'>JSON minify-stringify</p>
-				<a href='https://json-utility.vercel.app'>
+				<a href='https://github.com/heyiamZer0/json-utility'>
 					<svg
 						xmlns='http://www.w3.org/2000/svg'
 						width='26'
@@ -54,26 +70,24 @@ export const App = () => {
 			<div className='w-full h-full flex m-2 p-10 rounded-md drop-shadow-2xl'>
 				<div className='h-full w-full mx-5 text-sm text-black border border-gray-200 shadow-xl rounded-sm'>
 					<textarea
+					//@ts-ignore
+						onPaste={handlePaste}
 						autoFocus
 						ref={inputRef}
 						onChange={updateQuery}
 						className='bg-gray-100 no-scrollbar font-FiraMono text-xs outline-none focus:ring-2 focus:ring-indigo-400 p-10 px-4  h-full w-full'
 					/>
-					{isError ? <Error /> : null}
+					{isJSON ? null : <Error />}
 				</div>
 				<div className='h-full w-full mx-5 text-sm text-black border border-gray-200 shadow-xl rounded-sm'>
 					<textarea
 						value={isJSONCheck(query!) ? JSON.stringify(JSON.stringify(JSON.parse(query!))) : query}
 						className='bg-gray-100 no-scrollbar font-FiraMono text-xs outline-none focus:ring-2 focus:ring-indigo-400 p-10 px-4  h-full w-full'
 					/>
-
-					<CopyToClipboard
-						//@ts-ignore
-						text={query && JSON.stringify(JSON.stringify(JSON.parse(query)))}
-						onCopy={() => {
-							setState(true);
-						}}
-					>
+					
+					<CopyToClipboard 
+					//@ts-ignore
+					text={JSON.stringify(JSON.stringify(JSON.parse(json)))} onCopy={()=> {setState(true)}}>
 						<button className='relative -m-20 bottom-8 w-10 h-10'>
 							<svg
 								xmlns='http://www.w3.org/2000/svg'
@@ -83,9 +97,9 @@ export const App = () => {
 								stroke='currentColor'
 							>
 								<path
-									stroke-linecap='round'
-									stroke-linejoin='round'
-									stroke-width='2'
+									strokeLinecap='round'
+									strokeLinejoin='round'
+									strokeWidth='2'
 									d='M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z'
 								/>
 							</svg>
@@ -97,3 +111,5 @@ export const App = () => {
 		</div>
 	);
 };
+
+
